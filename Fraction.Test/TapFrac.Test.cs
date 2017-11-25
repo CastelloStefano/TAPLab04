7e0a1f87540842977796d9856c2a5ed843be9f1b
Stefano Castello <steisbetter@outlook.it>
2017-11-25 12:41:36 +0100

    Passed MCM & Sum Test

--- Fraction.Test/TapFrac.Test.cs
+++ Fraction.Test/TapFrac.Test.cs
@@ -1,9 +1,4 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fraction.Test
 {
@@ -11,21 +6,33 @@ namespace Fraction.Test
     public class TapFrac{
         private static readonly Fraction F1 = new Fraction(1, 2);
         private static readonly Fraction F2 = new Fraction(1, 6);
-        private readonly Fraction _f3 = F1 * F2;
-
+        private readonly Fraction _f3 = F1 * F2; // [1/12]
+        private readonly Fraction _f4 = F1 + F2; // [2/3]
 
         [Test]
         public void Moltip(){
-            Assert.That(_f3.num, Is.EqualTo(1));
-            Assert.That(_f3.den, Is.EqualTo(12));
+            Assert.That(_f3, Is.EqualTo(new Fraction(1, 12)));
         }
-
 
         [Test]
-        public void Eq()
-        {
+        public void Eq(){
             Assert.That(F1.Equals(new Fraction(1,2)),Is.EqualTo(true));
+            Assert.That(F2.Equals(new Fraction(1, 6)), Is.EqualTo(true));
         }
+
+        [Test]
+        public void Mcm()
+        {
+            Assert.That(Fraction.Mcm(2,5), Is.EqualTo(10));
+            Assert.That(Fraction.Mcm(2, 3), Is.EqualTo(6));
+            Assert.That(Fraction.Mcm(2, 4), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Sum(){
+            Assert.That(_f4.Equals(new Fraction(2,3)));
+        }
+
 
     }
 }
--- Fraction/Fraction.cs
+++ Fraction/Fraction.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fraction
 {
@@ -12,11 +8,42 @@ namespace Fraction
 
         public Fraction(int num, int den)
         {
+            if (den < 0)
+            {
+                den *= -1;
+                num *= -1;
+            }
+            for (int i = Math.Min(num, den); i > 1; i--)
+            {
+                if (num % i == 0 && den % i == 0)
+                {
+                    num /= i;
+                    den /= i;
+                }
+            }
+
             this.den = den;
             this.num = num;
         }
 
+        public static int Mcm(int n, int d){
+            if (n != d){
+                for (int i = 2; i <= (n * d); i++)
+                {
+                    if (i % n == 0 && i % d == 0) return i;
+                }
+                return  (n * d);
+            }
+            return n;
+        }
+
         public static Fraction operator *(Fraction f1, Fraction f2) => new Fraction(f1.num * f2.num, f1.den * f2.den);
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            int mcm = Mcm(f1.den, f2.den);
+            return new Fraction(((mcm/f1.den)*f1.num)+((mcm / f2.den) * f2.num),mcm);
+        }
 
         public override bool Equals(object obj)
         {
@@ -30,7 +57,7 @@ namespace Fraction
 
         public override int GetHashCode()
         {
-            return this.den.GetHashCode(); // errore del test ?
+            return (this.den.GetHashCode()+num.GetHashCode()).GetHashCode(); // Idea Giusta ?
         }
     }
 }
